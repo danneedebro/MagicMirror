2ee0009f4de1cae1d5eeb66331f005e03ea521f5
danneedebro <daniel.edebro@gmail.com>
2019-04-26 13:15:20 +0200

    Weather symbol now equal most frequent type and not the average

--- ModuleWeather.py
+++ ModuleWeather.py
@@ -5,6 +5,7 @@ import wx
 import wx.lib.stattext as ST
 import pytz
 from ModuleBase import ModuleBase
+from collections import Counter
 
 import json   # REMOVE
 
@@ -52,7 +53,7 @@ class ModuleWeather(ModuleBase):
         sizerMain.Add(weatherBoxNew, 0, wx.ALL, 3)
 
         weatherNext8h = self.Weather.GetMedianWeather("Göteborg", Now, 8)
-        symb = round(sum(weatherNext8h["Wsymb2"]["values"])/len(weatherNext8h["Wsymb2"]["values"]))
+        symb = max(Counter(weatherNext8h["Wsymb2"]["values"]))
         Tmin, Tmax = (min(weatherNext8h["t"]["values"]), max(weatherNext8h["t"]["values"]))
         pmean, pmax = (sum(weatherNext8h["pmean"]["values"]), max(weatherNext8h["pmax"]["values"]))
         wsMin, wsMax = (min(weatherNext8h["ws"]["values"]), max(weatherNext8h["ws"]["values"]))
@@ -61,7 +62,7 @@ class ModuleWeather(ModuleBase):
 
         dateTomorrow = (Now + timedelta(days=1)).replace(hour=9, minute=0, second=0, microsecond=0)
         weatherTomorrow = self.Weather.GetMedianWeather("Göteborg", dateTomorrow, 6)
-        symb = round(sum(weatherTomorrow["Wsymb2"]["values"])/len(weatherTomorrow["Wsymb2"]["values"]))
+        symb = max(Counter(weatherNext8h["Wsymb2"]["values"]))
         Tmin, Tmax = (min(weatherTomorrow["t"]["values"]), max(weatherTomorrow["t"]["values"]))
         pmean, pmax = (sum(weatherTomorrow["pmean"]["values"]), max(weatherTomorrow["pmax"]["values"]))
         wsMin, wsMax = (min(weatherTomorrow["ws"]["values"]), max(weatherTomorrow["ws"]["values"]))
